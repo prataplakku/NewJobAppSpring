d39a4b76b07fe7952208df44bc1536692ccfa8a3
prataplakku <prataplakku@gmail.com>
2025-05-20 17:31:16 -0400

    commit -m revampedrepo intial commit

---
+++ src/main/java/NewJob/NewJobApp/NewJobAppApplication.java
@@ -0,0 +1,13 @@
+package NewJob.NewJobApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewJobAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewJobAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NewJob/NewJobApp/NewJobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package NewJob.NewJobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewJobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
